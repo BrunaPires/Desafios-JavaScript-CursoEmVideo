509b25e82a84cf23877e8fbf31b737c638b49cac
BrunaPires <b.serip@gmail.com>
2020-12-17 17:03:06 -0300

    NewCommit

---
+++ Desafio-01/funcoes.js
@@ -0,0 +1,9 @@
+function clicou1() {
+    window.alert('Você clicou no 1° botão')
+}
+function clicou2() {
+    window.alert('Você clicou no 2° botão')
+}
+function clicou3() {
+    window.alert('Você clicou no 3° botão')
+}
