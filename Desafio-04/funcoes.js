e98f45b0b2ac3d3349fb544fb98fbe8710ff9d20
BrunaPires <b.serip@gmail.com>
2020-12-18 12:22:44 -0300

    Commit

---
+++ Desafio-04/funcoes.js
@@ -0,0 +1,18 @@
+
+
+function clicou() {
+
+    let v = ('')
+    let pg = ('')
+    let t = (pg) - (v)
+
+    let p = (window.prompt('Que produto você está comprando?'))
+  
+    let v = (window.prompt('Quanto custa ${p} que você está comprando?'))
+
+    let pg = (window.prompt('Qual foi o ${v} que você deu para pagar ${p}?'))
+
+
+
+    let = (window.alert(`Você comprou ${p} que custou ${v}. Deu valor ${pg} em dinheiro e vai receber ${t} de troco. Volte Sempre!'))
+}
