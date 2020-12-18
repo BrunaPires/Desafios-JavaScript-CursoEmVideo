6b45e231bb7ca13239310eaf92037f649ba981e5
BrunaPires <b.serip@gmail.com>
2020-12-18 18:31:45 -0300

    commit

--- Desafio-04/funcoes.js
+++ Desafio-04/funcoes.js
@@ -1,14 +1,14 @@
 function clicou() {
 
-  let n1 = (window.prompt('Que produto você está comprando?'))
+  let n1 = (window.prompt(`Que produto você está comprando?`))
 
-  let n2 = Number(window.prompt('Quanto custa ' + n1 + ' que você está comprando?'))
+  let n2 = Number(window.prompt(`Quanto custa ${n1} que você está comprando?`))
 
-  let n3 = Number(window.prompt('Qual foi o valor que você deu para pagar ' + n1 + '?'))
+  let n3 = Number(window.prompt(`Qual foi o valor que você deu para pagar ${n1}?`))
 
   calculo = n3 - n2
 
-  let = Number(window.alert('Você comprou ' + n1 + ' que custou ' + n2 + 
-  ' Deu ' + n3 + ' em dinheiro e vai receber ' + calculo + ' de troco ' +
-  'Volte sempre'))
+  let = Number(window.alert(`Você comprou ${n1} que custou ${n2}.
+Deu ${n3} em dinheiro e vai receber ${calculo} de troco.
+Volte sempre!`))
 }
