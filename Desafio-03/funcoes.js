0894b95014c1d834dbaf1601aa52aebe9f5ce26b
BrunaPires <b.serip@gmail.com>
2020-12-17 23:46:49 -0300

    NewCommit

---
+++ Desafio-03/funcoes.js
@@ -0,0 +1,13 @@
+
+
+function clicou() {
+
+    let n = (window.prompt('Digite um número inteiro qualquer'))
+  
+    let a = n - 1
+    let s = n * 1 + 1
+
+
+    let = (window.alert(`Antes de ${n} temos o número ${a}.
+Depois de ${n} temos o número ${s}`))
+}
