4f14c6d05c2e4079195b7f5e5e3ada90dc1537bd
BrunaPires <b.serip@gmail.com>
2020-12-18 00:08:10 -0300

    Commit

--- Desafio-02/funcoes.js
+++
@@ -1,8 +0,0 @@
-function clicou() {
-    let nome = window.prompt('Qual é o seu nome?')
-
-    let n1 = (window.prompt(`Olá, ${nome}! Quantos anos você tem?`))
-
-    let n2 = (window.alert(`Acabei de conhecer, ${nome} que tem ${n1} anos de idade!`))
-}
-
